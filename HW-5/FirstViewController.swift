44ae682f0152a4d2a31178be45cb6ec05f77cf2b
eltn <https://github.com/mkEldor/blackstone>
2018-06-29 15:14:54 +0600

    Initial Commit

---
+++ HW-5/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HW-5
+//
+//  Created by Eldor Makkambayev on 29.06.2018.
+//  Copyright © 2018 Eldor Makkambayev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ HW-5/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HW-5
+//
+//  Created by Eldor Makkambayev on 29.06.2018.
+//  Copyright © 2018 Eldor Makkambayev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
